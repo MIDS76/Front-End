f4399515e2cd8b74ff0f84104c3828bf759cd1da
Raquel <silvamonster10@gmail.com>
2025-11-19 15:23:13 -0300

    feature: criando models e telas para criar unidades curriculares. Alt… (#67)
    * feature: criando models e telas para criar unidades curriculares. Alterado: createButton/buttons/header/components, importarCSV/modal/components,importarUnidadeCurricular/modal, novaUnidadeCurricular/modal
    
    * feature: criando modal reutilizável para importar arquivos de lista CSV, reutilizando modal criado para importar arquivos CSV (unidades curriculares) na tela inicial do admin/pedagógico e criando opção para criar unidades curriculares manualmente na tela inicial do admin/pedagógico
    
    ---------
    
    Co-authored-by: GEOVANNA DE SOUSA KRUGER <geovanna_kruger@estudante.sesisenai.org.br>

---
+++ src/components/modal/novaUnidadeCurricular.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import Form from "next/form";
+import TextField from "../input/textField";
+import ActionModal from "./actionModal";
+import { toast } from "sonner";
+import { useState, useEffect } from "react";
+import { validateRequired } from "@/utils/formValidation";
+
+interface NovaUnidadeCurricularProps {
+  isOpen: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function NovaUnidadeCurricular({
+  isOpen,
+  setOpen,
+}: NovaUnidadeCurricularProps) {
+  const [nome, setNome] = useState("");
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+
+  const handleConfirm = () => {
+    const newErrors: { [key: string]: string } = {};
+    newErrors.nome = validateRequired(nome, "nome da Unidade Curricular");
+
+    if (newErrors.nome) {
+      setErrors(newErrors);
+      return;
+    }
+
+    toast.success("Unidade Curricular criada com sucesso!");
+
+    setTimeout(() => {
+      setOpen(false);
+      setNome("");
+      setErrors({});
+    }, 200);
+  };
+
+  useEffect(() => {
+    if (!isOpen) {
+      setTimeout(() => {
+        setNome("");
+        setErrors({});
+      }, 200);
+    }
+  }, [isOpen]);
+
+  return (
+    <ActionModal
+      isOpen={isOpen}
+      setOpen={setOpen}
+      title="Nova Unidade Curricular"
+      description=""
+      onClose={() => setOpen(false)}
+      onConfirm={handleConfirm}
+      conteudo={
+        <div className="space-y-4 max-w-md mx-auto">
+          <Form action={() => {}} className="flex flex-col gap-4">
+            <TextField
+              label="Nome"
+              placeholder="Insira o nome da nova Unidade Curricular"
+              type="text"
+              id="nomeUC"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              error={errors.nome} 
+            />
+          </Form>
+        </div>
+      }
+    />
+  );
+}
