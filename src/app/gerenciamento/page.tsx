3c1ffffc985c3e4caae206ef9a9746c4761de4fa
julia_fm_silva <julia_fm_silva@estudante.sesisenai.org>
2025-11-19 21:53:34 -0300

    criação do componente card SearchBar e ajuste na responsividade da tela de gerenciamento

--- src/app/gerenciamento/page.tsx
+++ src/app/gerenciamento/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import MedModal from "@/components/modal/medModal";
 import Lista from "@/components/lista/lista";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -10,90 +9,123 @@ import usuarios from "@/data/usuarios.json";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import SearchBar from "@/components/input/searchBar";
 import { useRouter } from "next/navigation";
-
+import InfoCard from "@/components/card/cardSearchBar";
 
 export default function GerenciamentoUsersTurmas() {
-
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
 
   const turmasArray = turmas;
   const usuariosArray = usuarios;
 
-  useEffect(() => {
-    return () => {
-      document.title = "Gerenciamento - ConselhEXPERT";
-    };
-  }, []);
-
   const router = useRouter();
+
+  useEffect(() => {
+    document.title = "Gerenciamento - Portal do Consel";
+  }, []);
 
   const handleTurmaClick = (id: number) => {
     router.push(`/gerenciamento/turma/${id}`);
   };
-  
+
   return (
-    <>
-      <ProtectedRoute>
-        <div className="flex flex-col lg:flex-row gap-6">
-          <section className="flex flex-col h-full gap-4 w-full py-8 lg:px-0 px-4 lg:w-1/2 ">
-            <div className="flex flex-col gap-6 md:mt-8 md:w-[450px] md:m-auto">
-              <h2 className="px-4 text-3xl font-title font-bold text-accent-foreground">
-                Gerenciar uma turma
-              </h2>
+    <ProtectedRoute>
+      <div
+        className="
+          mt-32 w-full grid gap-10
+          grid-cols-1
+          tablet:grid-cols-1
+          laptop:grid-cols-2
+          desktop:grid-cols-2
+          px-10
+          laptop:max-w-[1350px] laptop:mx-auto
+        "
+      >
+        {/* BLOCO TURMAS */}
+        <div className="w-full flex flex-col gap-6">
+          {/* InfoCard */}
+          <InfoCard
+            titulo="Gerenciamento de Turmas"
+            search={
+              <div className="mt-4">
+                <SearchBar
+                  placeholder="Procure por uma turma"
+                  searchQuery={searchQuery}
+                  setSearchQuery={setSearchQuery}
+                  filter
+                  filtrosMostrar={{
+                    aluno: false,
+                    turma: true,
+                    conselho: false,
+                  }}
+                />
+              </div>
+            }
+          />
 
-              <SearchBar
-                className="px-4"
-                placeholder="Buscar uma turma"
-                searchQuery={searchQuery}
-                setSearchQuery={setSearchQuery}
-                filter
-                filtrosMostrar={{ aluno: false, turma: true, conselho: false }}
-              />
-            </div>
-
-            <ScrollArea className="w-full h-[600px] mt-5 md:w-[450px] md:m-auto">
-              <div className="grid grid-cols-1 gap-6 px-4">
+          {/* CARD BRANCO + SCROLL */}
+          <div className="bg-white rounded-2xl shadow-lg border border-gray-200 p-4">
+            <ScrollArea className="h-[500px] tablet:h-[600px] laptop:h-[700px] desktop:h-[800px] w-full">
+              <div
+                className="
+                  grid 
+                  grid-cols-1 
+                  tablet:grid-cols-2 
+                  gap-4 
+                  px-4
+                  auto-rows-fr
+                "
+              >
                 {turmasArray?.map((classItem, index) => (
-                  <MedModal
-                    key={index}
-                    courseCode={classItem.codigoTurma}
-                    courseName={classItem.nomeCurso}
-                    onClick={() => handleTurmaClick(classItem.id)}
-                    simple
-                  />
+                  <div className="h-32" key={index}>
+                    <MedModal
+                      courseCode={classItem.codigoTurma}
+                      courseName={classItem.nomeCurso}
+                      onClick={() => handleTurmaClick(classItem.id)}
+                      simple
+                    />
+                  </div>
                 ))}
               </div>
             </ScrollArea>
-          </section>
+          </div>
+        </div>
 
-          <section className="flex flex-col justify-between gap-4 w-full py-8 lg:py-8 md:px-14 
-            lg:px-0 px-4 lg:w-1/2 h-full md:mb-8">
-            <div className="flex flex-col gap-6 md:mt-8 lg:pr-28">
-              <h2 className="px-4 text-3xl font-title font-bold text-accent-foreground">
-                Gerenciar Usuários
-              </h2>
-              <SearchBar
-                className="px-4"
-                placeholder="Buscar um usuário"
-                searchQuery={searchQuery}
-                setSearchQuery={setSearchQuery}
-                filter
-                filtrosMostrar={{ aluno: true, turma: false, conselho: false }}
-              />
-            </div>
-            <div className="lg:pr-28">
+        {/* BLOCO USUÁRIOS */}
+        <div className="w-full flex flex-col gap-6">
+          {/* InfoCard */}
+          <InfoCard
+            titulo="Gerenciamento de Usuários"
+            search={
+              <div className="mt-4">
+                <SearchBar
+                  placeholder="Procure por um usuário"
+                  searchQuery={searchQuery}
+                  setSearchQuery={setSearchQuery}
+                  filter
+                  filtrosMostrar={{
+                    aluno: false,
+                    turma: true,
+                    conselho: false,
+                  }}
+                />
+              </div>
+            }
+          />
+
+          {/* CARD BRANCO + SCROLL */}
+          <div className="bg-white rounded-2xl shadow-lg border border-gray-200 p-4">
+            <ScrollArea className="h-[500px] tablet:h-[500px] laptop:h-[418px] desktop:h-[480px] w-full">
               <Lista
                 isDialogOpen={isDialogOpen}
                 setIsDialogOpen={setIsDialogOpen}
                 tipo="edit"
                 usuarios={usuariosArray}
-                className="px-4"
               />
-            </div>
-          </section>
+            </ScrollArea>
+          </div>
         </div>
-      </ProtectedRoute>
-    </>
+      </div>
+    </ProtectedRoute>
   );
 }
