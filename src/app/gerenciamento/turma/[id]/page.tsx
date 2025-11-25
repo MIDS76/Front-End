ac98ba1e9718a5dbec87c1cbcbfefb43b8b9b6e9
GEOVANNA DE SOUSA KRUGER <geovanna_kruger@estudante.sesisenai.org.br>
2025-11-24 21:50:42 -0300

    fix: começando a ajustar a tela de gerenciar turmas, falta a parte de gerenciar e listar usuários. gerenciamento/turma/page.

--- src/app/gerenciamento/turma/[id]/page.tsx
+++ src/app/gerenciamento/turma/[id]/page.tsx
@@ -4,35 +4,52 @@ import { toast } from "sonner";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import usuarios from "@/data/usuarios.json";
 import turmas from "@/data/turma.json";
-import { useParams} from "next/navigation";
+import { useParams } from "next/navigation";
 import TurmaForm from "@/components/turma/TurmaForm";
 import { useRouter } from "next/navigation";
+import InfoCard from "@/components/card/cardTituloTelas";
 
-export default function GereciarTurma() {
+export default function GerenciarTurma() {
   const usuariosArray = usuarios;
   const turmasArray = turmas;
   const { id } = useParams();
-
   const router = useRouter();
+
   const alunos = usuariosArray.filter((user) => user.role === "Aluno");
   const turmaId = Number(id);
   const turma = turmasArray.find((t) => t.id === turmaId);
 
   return (
     <ProtectedRoute>
-      <TurmaForm
-        title="Gerenciar Turma"
-        initialData={{
-          codigoTurma: turma?.codigoTurma ?? "",
-          nomeCurso: turma?.nomeCurso ?? "",
-          dataInicio: turma?.dataInicio ?? "",
-          dataFim: turma?.dataFim ?? "",
-        }}
-        alunos={alunos}
-        onSubmit={(form, alunosSelecionados) => {
-          toast.success("Turma atualizada com sucesso!");
-        }}
-      />
+      <div className="flex w-full min-h-screen p-6 gap-6 
+      desktop:pt-[9rem] desktop:px-16
+      laptop:pt-[6rem] laptop:px-12
+      tablet:pt-[6rem]">
+        {/* Coluna Esquerda card+form */}
+        <div className="flex flex-col gap-6 w-full laptop:w-1/2">
+          <InfoCard
+            titulo="Gerenciamento de turma"
+            subtitulo="Status"
+            descricao="Ativa"
+          />
+
+          <TurmaForm
+            title="Gerenciar Turma"
+            initialData={{
+              codigoTurma: turma?.codigoTurma ?? "",
+              nomeCurso: turma?.nomeCurso ?? "",
+              dataInicio: turma?.dataInicio ?? "",
+              dataFim: turma?.dataFim ?? "",
+            }}
+            onSubmit={() => {
+              toast.success("Turma atualizada com sucesso!");
+            }}
+          />
+        </div>
+
+        {/* futuro modal de lista - aguardando commit da Júlia Macena */}
+        <div className="hidden laptop:flex w-1/2"></div>
+      </div>
     </ProtectedRoute>
   );
 }
