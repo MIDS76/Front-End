4360d1062a4e38c04c4441eeebb21ba952d09927
julia_fm_silva <julia_fm_silva@estudante.sesisenai.org>
2025-12-03 21:51:07 -0300

    preferencias de configuração do usuario salvas o LocalStorage

--- src/components/header/buttons/ConfigurationButton.tsx
+++ src/components/header/buttons/ConfigurationButton.tsx
@@ -1,6 +1,25 @@
+"use client";
+
 import { ConfigDialog } from "@/components/perfil/configDialog";
 
-
 export default function ConfigurationButton() {
-    return <ConfigDialog />;
+  let userId: string | number |null = null;
+
+    if (typeof window !== "undefined") {
+        const storedUser = localStorage.getItem("user");
+        const user = storedUser ? JSON.parse(storedUser) : null;
+        userId = user ? user.id : null;
+    }
+
+        const handlePasswordChange = () => {
+                console.log("Abrir modal de alteração de senha");
+        };
+
+    
+    return (
+        <ConfigDialog
+            userId={userId}
+            onChangePassword={handlePasswordChange} // Ensure ConfigDialogProps includes this prop
+        />
+    );
 }
